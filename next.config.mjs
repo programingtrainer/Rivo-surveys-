6d8cb8d872c7b61dfcf3025fb01fd5ab07a1ef00
programingtrainer <programingtrainer2@gmail.com>
2026-09-08 15:44:22 +0300

    Fix pg-cloudflare file tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**"
+    ]
+  }
 };
 
 export default nextConfig;
